f28f0e92b9265aea7bc9bcaf91600f62b589624c
Alan Arriaga <A14N.arriaga@gmail.com>
2021-04-11 17:16:22 -0500

    Implement Flexbox

--- code/script.ts
+++ code/script.ts
@@ -1,15 +1,34 @@
 import "./style.scss"
 
-// <section class="card">
-//                     <img src="" alt="">
-//                     <div class="content">
-//                         <div class="title">
-//                             <h1>Name</h1>
-//                             <span>✅</span>
-//                         </div>
-//                         <h2>Nickname</h2>
-//                         <p>Birthday</p>
-//                         <p>Ocupation</p>
-//                         <p>Portrayed</p>
-//                     </div>
-//                 </section>
+const cards = document.getElementById('cards') as HTMLElement
+
+const callAPI = async () => {
+    const getData = await fetch('https://www.breakingbadapi.com/api/characters')
+    const data = await getData.json()
+    const result = data
+    .map((character) => createCard(character))
+    .join(' ')
+    cards.innerHTML = result
+}
+
+const createCard = ({img,name,status,nickname,birthday,occupation,portrayed}): string => {
+    return `
+    <section class="card">
+        <img src="${img}" alt="${name}">
+        <div class="content">
+            <div class="title">
+                <h1>${name}</h1>
+                <span>${(status === 'Alive') ? "✅" : "❎"}</span>
+            </div>
+            <h2>${nickname}</h2>
+            <p>${birthday}</p>
+            <p>${occupation}</p>
+            <p>${portrayed}</p>
+        </div>
+    </section>
+    `
+}
+
+callAPI()
+
+
