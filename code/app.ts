9be9588badcaad84010744345fd0503f0a1e0b82
Alan Arriaga <A14N.arriaga@gmail.com>
2021-04-12 10:05:34 -0500

    img height fixed

--- code/app.ts
+++ code/app.ts
@@ -19,7 +19,9 @@ const callAPI = async () => {
 const createCard = ({img,name,status,nickname,birthday,occupation,portrayed}): string => {
     return `
     <section class="card">
-        <img src="${img}" alt="${name}">
+        <div class="img-content">
+            <img src="${img}" alt="${name}">
+        </div>
         <div class="content">
             <div class="title">
                 <h1>${name}</h1>
